5fa7e2492d0ece6d63848c8153c93ef0a53216ea
manueledigregorio <dig.manuele@gmail.com>
2024-03-06 13:01:46 +0100

    [add] search

--- src/data/store.js
+++ src/data/store.js
@@ -3,6 +3,7 @@ import { reactive } from "vue";
 export const store = reactive({
   apiUrl: 'https://api.rawg.io/api/',
   key:'f5b78cfda1374e69a25202d24ee3bd2c',
+ //https://api.rawg.io/api/games?key=f5b78cfda1374e69a25202d24ee3bd2c&tags=multiplayer&search=Zelda tears of the kingdom
   search: null,
   arrayResult:[],
   gameDetail:{},
